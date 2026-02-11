d8ca4cfa153414b1e6c1ae2ffb25afdc34a8e955
Diogod01 <simonminchev1@abv.bg>
2026-02-11 12:16:29 +0200

    Add TaskItem.cs

---
+++ TaskItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TaskManager.Models
+{
+    public class TaskItem
+    {
+        public string Name { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public TaskItem(string name)
+        {
+            Name = name;
+            IsCompleted = false;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}|{1}", Name, IsCompleted);
+        }
+
+        public static TaskItem FromFile(string line)
+        {
+            string[] parts = line.Split('|');
+            return new TaskItem(parts[0])
+            {
+                IsCompleted = bool.Parse(parts[1])
+            };
+        }
+    }
+}
